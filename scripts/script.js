9c4feeff114b5e5b08e1b424a02c53d1668a77e2
MichaelWilhelm1 <90198801+MichaelWilhelm1@users.noreply.github.com>
2021-09-22 21:58:07 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+var Hamburgermenu = document.querySelector(".toggle-button");
+var navigatie = document.querySelector(".links");
+
+Hamburgermenu.addEventListener("click", openmenu);
+
+function openmenu() {
+    if (navigatie.style.display === "none") {
+        navigatie.style.display = "block";
+    } else {
+        navigatie.style.display = "none";
+    }
+
+
+}
